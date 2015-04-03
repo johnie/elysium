db8d052e49343deab5c1899af4f70e572771cc97
Johnie Hjelm <johnie.hjelm@symbio.com>
2015-04-03 23:25:27 +0200

    …

--- inc/form.php
+++ inc/form.php
@@ -10,7 +10,7 @@
 		}
 		if ( trim( $_POST['efternamn'] ) === '' ) {
 			$efterNameError = 'Vänligen fyll i ditt efternamn';
-			$hasError§ = true;
+			$hasError = true;
 		}
 
 		$name = $_POST['fornamn'] . ' ' . $_POST['efternamn'];
@@ -51,41 +51,39 @@
 <form action="" id="elysium" method="POST">
 	<fieldset>
 		<legend><?php _e("Personnummer", "elysium"); ?> <span class="asterisk">*</span></legend>
-		<input type="text" name="elysium_personnr" id="personnr" class="required">
 		<label for="personnr"><?php _e("Ange ditt personnummer [ÅÅMMDD-1234]", "elysium"); ?></label>
+		<input type="text" name="elysium_personnr" id="personnr" class="required">
 	</fieldset>
 	<fieldset>
 		<legend><?php _e("Namn", "elysium"); ?> <span class="asterisk">*</span></legend>
-		<input type="text" name="fornamn" id="fornamn" class="required">
 		<label for="fornamn"><?php _e("Förnamn", "elysium"); ?></label>
+		<input type="text" name="fornamn" id="fornamn" class="required">
 		<?php if ( $forNameError != ''): ?>
 			<span class="error"><?php echo $forNameError; ?></span>
 		<?php endif; ?>
-		<input type="text" name="efternamn" id="efternamn" class="required">
 		<label for="fornamn"><?php _e("Efternamn", "elysium"); ?></label>
+		<input type="text" name="efternamn" id="efternamn" class="required">
 		<?php if ( $efterNameError != ''): ?>
 			<span class="error"><?php echo $efterNameError; ?></span>
 		<?php endif; ?>
 	</fieldset>
 	<fieldset>
 		<legend><?php _e("Adress", "elysium"); ?> <span class="asterisk">*</span></legend>
-		<input type="text" name="elysium_gatuadress" id="gatuadress" class="required">
 		<label for="gatuadress"><?php _e("Gatuadress", "elysium"); ?></label>
-		<input type="text" name="elysium_stad" id="stad" class="required">
+		<input type="text" name="elysium_gatuadress" id="gatuadress" class="required">
 		<label for="stad"><?php _e("Stad", "elysium"); ?></label>
-		<input type="text" name="elysium_postnr" id="postnr" class="required">
+		<input type="text" name="elysium_stad" id="stad" class="required">
 		<label for="postnr"><?php _e("Postnr", "elysium"); ?></label>
+		<input type="text" name="elysium_postnr" id="postnr" class="required">
 	</fieldset>
 	<fieldset>
 		<legend><?php _e("Kontakt", "elysium"); ?></legend>
-		<input type="text" name="elysium_mobiltelefon" id="mobiltelefon">
 		<label for="mobiltelefon"><?php _e("Mobiltelefon", "elysium"); ?></label>
-		<input type="text" name="elysium_hemtelefon" id="hemtelefon">
+		<input type="text" name="elysium_mobiltelefon" id="mobiltelefon">
 		<label for="hemtelefon"><?php _e("Hemtelefon", "elysium"); ?></label>
-		<input type="email" name="elysium_epost" id="epost">
+		<input type="text" name="elysium_hemtelefon" id="hemtelefon">
 		<label for="epost"><?php _e("E-post", "elysium"); ?></label>
-		<input type="email" name="elysium_epost2" id="epost2">
-		<label for="epost2"><?php _e("Bekräfta E-post", "elysium"); ?></label>
+		<input type="email" name="elysium_epost" id="epost">
 	</fieldset>
 	<fieldset>
 		<input type="hidden" name="submitted" id="submitted" value="true" />
