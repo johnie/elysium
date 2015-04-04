d585357747c501f3fbc85f0f4fda445eb2150267
Johnie Hjelm <johnie.hjelm@symbio.com>
2015-04-04 20:30:40 +0200

    Minor fixes

--- inc/form.php
+++ inc/form.php
@@ -48,46 +48,82 @@
 	}
 ?>
 
-<form action="" id="elysium" method="POST">
+<form action="" id="elysium" class="medlem-form" method="POST">
 	<fieldset>
 		<legend><?php _e("Personnummer", "elysium"); ?> <span class="asterisk">*</span></legend>
-		<label for="personnr"><?php _e("Ange ditt personnummer [ÅÅMMDD-1234]", "elysium"); ?></label>
-		<input type="text" name="elysium_personnr" id="personnr" class="required">
+		<div class="medlem-input">
+			<input type="text" class="input__field--isao" name="elysium_personnr" id="personnr" maxlength="11" required placeholder="<?php _e("921224-1337", "elysium"); ?>">
+			<label for="personnr" class="input__label--isao" data-content="<?php _e("Personnummer [ÅÅMMDD-1234]", "elysium"); ?>">
+				<span class="input__label-content--isao"><?php _e("Personnummer [ÅÅMMDD-1234]", "elysium"); ?></span>
+			</label>
+		</div>
 	</fieldset>
 	<fieldset>
 		<legend><?php _e("Namn", "elysium"); ?> <span class="asterisk">*</span></legend>
-		<label for="fornamn"><?php _e("Förnamn", "elysium"); ?></label>
-		<input type="text" name="fornamn" id="fornamn" class="required">
+		<div class="medlem-input">
+			<input type="text" class="input__field--isao" name="fornamn" id="fornamn" required placeholder="<?php _e("John", "elysium"); ?>">
+			<label for="fornamn" class="input__label--isao" data-content="<?php _e("Förnamn", "elysium"); ?>">
+				<span class="input__label-content--isao"><?php _e("Förnamn", "elysium"); ?></span>
+			</label>
+		</div>
 		<?php if ( $forNameError != ''): ?>
 			<span class="error"><?php echo $forNameError; ?></span>
 		<?php endif; ?>
-		<label for="fornamn"><?php _e("Efternamn", "elysium"); ?></label>
-		<input type="text" name="efternamn" id="efternamn" class="required">
+		<div class="medlem-input">
+			<input type="text" class="input__field--isao" name="efternamn" id="efternamn" required placeholder="<?php _e("Doe", "elysium"); ?>">
+			<label for="efternamn" class="input__label--isao" data-content="<?php _e("Efternamn", "elysium"); ?>">
+				<span class="input__label-content--isao"><?php _e("Efternamn", "elysium"); ?></span>
+			</label>
+		</div>
 		<?php if ( $efterNameError != ''): ?>
 			<span class="error"><?php echo $efterNameError; ?></span>
 		<?php endif; ?>
 	</fieldset>
 	<fieldset>
 		<legend><?php _e("Adress", "elysium"); ?> <span class="asterisk">*</span></legend>
-		<label for="gatuadress"><?php _e("Gatuadress", "elysium"); ?></label>
-		<input type="text" name="elysium_gatuadress" id="gatuadress" class="required">
-		<label for="stad"><?php _e("Stad", "elysium"); ?></label>
-		<input type="text" name="elysium_stad" id="stad" class="required">
-		<label for="postnr"><?php _e("Postnr", "elysium"); ?></label>
-		<input type="text" name="elysium_postnr" id="postnr" class="required">
+		<div class="medlem-input">
+			<input type="text" class="input__field--isao" name="elysium_gatuadress" id="gatuadress" required placeholder="<?php _e("Närgången 1", "elysium"); ?>">
+			<label for="gatuadress" class="input__label--isao" data-content="<?php _e("Gatuadress", "elysium"); ?>">
+				<span class="input__label-content--isao"><?php _e("Gatuadress", "elysium"); ?></span>
+			</label>
+		</div>
+		<div class="medlem-input">
+			<input type="text" class="input__field--isao" name="elysium_stad" id="stad" required placeholder="<?php _e("Göteborg", "elysium"); ?>">
+			<label for="stad" class="input__label--isao" data-content="<?php _e("Stad", "elysium"); ?>">
+				<span class="input__label-content--isao"><?php _e("Stad", "elysium"); ?></span>
+			</label>
+		</div>
+		<div class="medlem-input">
+			<input type="text" class="input__field--isao" name="elysium_postnr" id="postnr" required placeholder="<?php _e("417 56", "elysium"); ?>">
+			<label for="postnr" class="input__label--isao" data-content="<?php _e("Postnr", "elysium"); ?>">
+				<span class="input__label-content--isao"><?php _e("Postnr", "elysium"); ?></span>
+			</label>
+		</div>
 	</fieldset>
 	<fieldset>
 		<legend><?php _e("Kontakt", "elysium"); ?></legend>
-		<label for="mobiltelefon"><?php _e("Mobiltelefon", "elysium"); ?></label>
-		<input type="text" name="elysium_mobiltelefon" id="mobiltelefon">
-		<label for="hemtelefon"><?php _e("Hemtelefon", "elysium"); ?></label>
-		<input type="text" name="elysium_hemtelefon" id="hemtelefon">
-		<label for="epost"><?php _e("E-post", "elysium"); ?></label>
-		<input type="email" name="elysium_epost" id="epost">
+		<div class="medlem-input">
+			<input type="text" class="input__field--isao" name="elysium_mobiltelefon" id="mobiltelefon" placeholder="<?php _e("073- 999 13 37", "elysium"); ?>">
+			<label for="mobiltelefon" class="input__label--isao" data-content="<?php _e("Mobiltelefon", "elysium"); ?>">
+				<span class="input__label-content--isao"><?php _e("Mobiltelefon", "elysium"); ?></span>
+			</label>
+		</div>
+		<div class="medlem-input">
+			<input type="text" class="input__field--isao" name="elysium_hemtelefon" id="hemtelefon" placeholder="<?php _e("08 – 685 80 80", "elysium"); ?>">
+			<label for="hemtelefon" class="input__label--isao" data-content="<?php _e("Hemtelefon", "elysium"); ?>">
+				<span class="input__label-content--isao"><?php _e("Hemtelefon", "elysium"); ?></span>
+			</label>
+		</div>
+		<div class="medlem-input">
+			<input type="email" class="input__field--isao" name="elysium_epost" id="epost" placeholder="<?php _e("john.doe@gmail.com", "elysium"); ?>">
+			<label for="epost" class="input__label--isao" data-content="<?php _e("E-post", "elysium"); ?>">
+				<span class="input__label-content--isao"><?php _e("E-post", "elysium"); ?></span>
+			</label>
+		</div>
 	</fieldset>
 	<fieldset>
 		<input type="hidden" name="submitted" id="submitted" value="true" />
-		<button type="submit"><?php _e("Bli medlem nu", "elysium"); ?></button>
+		<button type="submit" class="button--wayra"><?php _e("Bli medlem nu", "elysium"); ?></button>
 		<?php wp_nonce_field( 'post_nonce', 'post_nonce_field' ); ?>
 	</fieldset>
 </form>
